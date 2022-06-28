0535032144bc1bb672f79c6346b1b315d656a713
Helby <helbyarbul@gmail.com>
2022-06-28 12:27:07 +0700

    membuat backend

---
+++ index.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const app = express()
+const port = 5000
+
+//connection url
+const url = 'mongodb+srv://nightmoon:Polopo999@nightmare.lz7lo.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+
+//database name
+const dbName = 'backend';
+
+app.get('/', async(req,res) => {
+    await client.connect();
+    console.log('Connected success');
+    const db = client.db(dbName);
+    const collection = db.collection('portofolio');
+    const findResult = await collection.find({}).toArray();
+    res.status(200).json({Datajson : findResult})
+})
+
+app.listen(port, () => {
+    console.log(`Example listen on port ${port}`)
+})
